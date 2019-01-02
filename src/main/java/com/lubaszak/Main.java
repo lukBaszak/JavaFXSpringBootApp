1571460ffdc5edf331a117ad76e00d5e8cd093e8
lukBaszak <43365577+lukBaszak@users.noreply.github.com>
2019-01-02 22:08:17 +0100

    for now it's compiling :)

--- src/main/java/com/lubaszak/Main.java
+++ src/main/java/com/lubaszak/Main.java
@@ -7,7 +7,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import com.lubaszak.config.StageManager;
-import com.lubaszak.view.FxmlView;
+import com.lubaszak.utilities.FxmlView;
 
 @SpringBootApplication
 public class Main extends Application {
