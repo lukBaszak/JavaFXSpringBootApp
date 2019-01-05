baf14b028a9ead31a65c8f9aeaa6b99dc21737ea
lukBaszak <43365577+lukBaszak@users.noreply.github.com>
2019-01-05 14:50:11 +0100

    Replaced 3 outer classes with 1 with 2 inner

--- src/main/java/com/lubaszak/bean/ProductResponse.java
+++ src/main/java/com/lubaszak/bean/ProductResponse.java
@@ -26,4 +26,71 @@ public class ProductResponse {
     public void setCommon(CommonFoodInfo[] common) {
         this.common = common;
     }
+
+
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class BrandedProductInfo {
+
+        @JsonProperty("food_name")
+        public String foodName;
+        @JsonProperty("serving_unit")
+        public String servingUnit;
+        @JsonProperty("nix_brand_id")
+        public String nixBrandId;
+        @JsonProperty("brand_name_item_name")
+        public String brandNameItemName;
+        @JsonProperty("serving_qty")
+        public Integer servingQty;
+        @JsonProperty("nf_calories")
+        public Integer nfCalories;
+        @JsonProperty("brand_name")
+        public String brandName;
+        @JsonProperty("brand_type")
+        public Integer brandType;
+        @JsonProperty("nix_item_id")
+        public String nixItemId;
+
+
+        public String getFoodName() {
+            return foodName;
+        }
+        public String getServingUnit() {
+            return servingUnit;
+        }
+        public String getNixBrandId() {
+            return nixBrandId;
+        }
+        public String getBrandNameItemName() {
+            return brandNameItemName;
+        }
+        public Integer getServingQty() {
+            return servingQty;
+        }
+        public Integer getNfCalories() {
+            return nfCalories;
+        }
+        public String getBrandName() {
+            return brandName;
+        }
+        public Integer getBrandType() {
+            return brandType;
+        }
+        public String getNixItemId() {
+            return nixItemId;
+        }
+    }
+
+     public static class CommonFoodInfo {
+
+        @JsonProperty("food_name")
+        private String foodName;
+
+        public String getFoodName() {
+            return foodName;
+        }
+        public void setFoodName(String foodName) {
+            this.foodName = foodName;
+        }
+    }
 }
